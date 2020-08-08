0960efa08a8ae4e1d5c6e7c953f3d59a2ac1a0a0
ToTheHit <dmtshkv@gmail.com>
2020-08-09 00:06:00 +0300

    Updated GitHub Pages version to 0.2

---
+++ precache-manifest.58e553422417b5eec793091ddbc48f83.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37477e7cedc16f216a140865de1630c1",
+    "url": "/Battlefilms/index.html"
+  },
+  {
+    "revision": "dca32e93cbd5ad058950",
+    "url": "/Battlefilms/static/css/2.1e562f28.chunk.css"
+  },
+  {
+    "revision": "01a277f66845dad1ba8d",
+    "url": "/Battlefilms/static/css/main.5e460a38.chunk.css"
+  },
+  {
+    "revision": "dca32e93cbd5ad058950",
+    "url": "/Battlefilms/static/js/2.14b5da6d.chunk.js"
+  },
+  {
+    "revision": "8cbce3d9d5df4ad58d7c3cdaa6fc70d2",
+    "url": "/Battlefilms/static/js/2.14b5da6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a277f66845dad1ba8d",
+    "url": "/Battlefilms/static/js/main.9627511c.chunk.js"
+  },
+  {
+    "revision": "08eea36dcfad0f85dd03",
+    "url": "/Battlefilms/static/js/runtime-main.c9184682.js"
+  },
+  {
+    "revision": "26512706e3f45cf6d011bd1f9ef0df8e",
+    "url": "/Battlefilms/static/media/TTCommons-DemiBold.26512706.woff"
+  },
+  {
+    "revision": "71ed90b3285019275ff20a8b57e194d5",
+    "url": "/Battlefilms/static/media/intro-logo-dark.71ed90b3.png"
+  },
+  {
+    "revision": "7d8b4497028f0ea7097abf7ede196f5c",
+    "url": "/Battlefilms/static/media/intro-logo-light.7d8b4497.png"
+  },
+  {
+    "revision": "e2716b68611a1827acf3a37ff551ed14",
+    "url": "/Battlefilms/static/media/intro-market-dark_new.e2716b68.png"
+  },
+  {
+    "revision": "4fb1fc62687e3baf7156abcf9a2e9c56",
+    "url": "/Battlefilms/static/media/intro-market-light_new.4fb1fc62.png"
+  },
+  {
+    "revision": "b0356757453a50ec41ae0bd9f78e00f2",
+    "url": "/Battlefilms/static/media/intro-poll-dark.b0356757.png"
+  },
+  {
+    "revision": "1709f6a38e53235e077c2ad7a4e33830",
+    "url": "/Battlefilms/static/media/intro-poll-light.1709f6a3.png"
+  },
+  {
+    "revision": "20ad7a2af0ee1f803d72bb00d4965239",
+    "url": "/Battlefilms/static/media/opt_cherry-fail.20ad7a2a.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.af23adb14ce1934a0b07388c8f599dbe.js"
+  "/Battlefilms/precache-manifest.58e553422417b5eec793091ddbc48f83.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Battlefilms/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
